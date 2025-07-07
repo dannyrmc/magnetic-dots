fd58b8e03dcdbcd2f378de1e668ddd9885d860dc
Daniel McNamara <dannymc.code@gmail.com>
2025-07-06 22:20:44 -0400

    added basic og metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { siteConfig } from "../config/site";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,8 +14,53 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Magnetic Dots",
-  description: "Animating magnetic dots",
+  title: siteConfig.title,
+  description: siteConfig.description,
+  authors: [
+    {
+      name: siteConfig.name,
+    },
+  ],
+  creator: siteConfig.name,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: siteConfig.title,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: `@${siteConfig.username}`,
+  },
+  icons: {
+    icon: [
+      { url: "/icons/favicon_192.png" },
+      { url: "/icons/favicon_512.png" },
+    ],
+    apple: [
+      { url: "/icons/favicon_180.png" },
+      { url: "/icons/favicon_180.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "apple-touch-icon-precomposed",
+        url: "/icons/favicon_180.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ config/site.js
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "Danny Mc",
+  title: "Magnetic Dots",
+  url: "https://magnetic-dots.vercel.app/",
+  email: "dannymc.talk@gmail.com",
+  username: "dannymc",
+  ogImage: "https://magnetic-dots.vercel.app/og.jpg",
+  description:
+    "Interactive and adjustable number dot matrix animation.",
+  keywords: [
+    "next.js",
+    "dots",
+    "interactive",
+  ],
+};
